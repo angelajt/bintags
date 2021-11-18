f715d8720c5fb584c3eb9dd42da39c7cc97198e9
angelajt <angela@t7a.org>
2021-11-17 17:04:39 -0800

    improve pi code

--- circuitpython/pi/pimqtt.py
+++ circuitpython/pi/pimqtt.py
@@ -50,6 +50,10 @@ display.show()
 width = display.width
 height = display.height
 
+#
+# LoRa Setup
+#
+
 # Configure LoRa Radio
 CS = DigitalInOut(board.CE1)
 RESET = DigitalInOut(board.D25)
@@ -58,68 +62,48 @@ rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
 rfm9x.tx_power = 23
 prev_packet = None
 
-#
-# MQTT Setup
-#
+# convert a packet to a parsed message
+def toMsg(data):
+    msg = {
+            "type": data[0],
+            "from": data[1],
+            "to": data[2],
+            "counter": data[3],
+            "ordernum": data[4],
+            "content": data[5]
+            }
+    return msg
 
-send_topic = "to-cli" # pi -> cli
-recv_topic = "from-cli" # cli -> pi
+def toAck(data):
+     ack = {
+            "type": data[0],
+            "from": data[1],
+            "to": data[2],
+            "counter": data[3],
+            "ordernum": data[4],
+            "content": data[5]
+            }
+     return ack
 
-# set up log file and store in current directory
-
-d = os.path.join(os.environ['PWD'], ".mqtt_logs")
-histfn = os.path.join(d, ".history")
-
-if not os.path.exists(d):
-    os.mkdir(d)
-try:
-    readline.read_history_file(histfn)
-except IOError:
-    pass
-atexit.register(readline.write_history_file, histfn)
-
-# mqtt connection
-
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
-    client.subscribe(recv_topic)
-
-def on_message(client, userdata, msg):
-    print(str(msg.payload))
-
-def _exit(rc):
-    sys.exit(rc)
-
-def send(line):
-    client.publish(send_topic, payload=line)
-
-
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-client.connect("lorapi.local", 1883, 60)
-client.loop_start()
-
-counter = 0
-while True:
-    packet = None
-    # draw a box to clear the image
-    display.fill(0)
-    display.text('RasPi LoRa', 35, 0, 1)
-
+def receive():
     # check for packet rx
     packet = rfm9x.receive(timeout=5)
     if packet is None:
         display.show()
         display.text('- Waiting for PKT -', 15, 20, 1)
     else:
-        # Display the packet text and rssi
         display.fill(0)
-        prev_packet = packet
-        packet_text = str(prev_packet, "utf-8")
+
+        packet_text = str(packet, "utf-8")
         data = packet_text.split(", ")
         print(data)
-        if data[0] == "MSG" and data[2] == "pi":
+
+        try:
+            msg = toMsg(data)
+        except IndexError: # the message is an ack, don't receive
+            return
+
+        if msg["to"] == "pi":
             time.sleep(2)
             ack = "ACK, " + data[2] + ", " + data[1] + ", " + data[3]
             rfm9x.send(bytes(ack, "utf-8"), destination=255, node=255)
@@ -130,36 +114,65 @@ while True:
             display.text('RX: ', 0, 0, 1)
             display.text(packet_text, 25, 0, 1)
 
-    if not btnA.value:
-        while True:
-            # Send Button A
-            display.fill(0)
-            button_a_data = bytes("MSG, pi, m4-1, " + str(counter) + ", 12345","utf-8")
-            rfm9x.send(button_a_data)
-            display.text('Sent Button A!', 25, 15, 1)
-            time.sleep(0.1)
-            packet = rfm9x.receive(timeout=10.0)
-            if packet is not None:
-                packet_text = str(packet, "ascii")
-                data = packet_text.split(", ")
-                if data[0] == "ACK" and data[2] == "pi" and data[3] == str(counter):
-                    print("ack received!")
-                    counter += 1
-                    break
-            print("no ack received! trying again")
-    elif not btnB.value:
-        # Send Button B
-        display.fill(0)
-        button_b_data = bytes("22222","utf-8")
-        rfm9x.send(button_b_data)
-        display.text('Sent Button B!', 25, 15, 1)
-    elif not btnC.value:
-        # Send Button C
-        display.fill(0)
-        button_c_data = bytes("99999","utf-8")
-        rfm9x.send(button_c_data)
-        display.text('Sent Button C!', 25, 15, 1)
-
-
-    display.show()
+def sendLora(num):
+    global counter
+    data = bytes("MSG, pi, m4-1, " + str(counter) + ", " + num,"utf-8")
+    rfm9x.send(data)
     time.sleep(0.1)
+    packet = rfm9x.receive(timeout=10.0)
+    if packet is not None:
+        packet_text = str(packet, "ascii")
+        data = packet_text.split(", ")
+        if data[0] == "ACK" and data[2] == "pi" and data[3] == str(counter):
+            print("ack received!")
+            counter += 1
+            return
+    print("no ack received! trying again")
+
+
+#
+# MQTT Setup
+#
+
+send_topic = "to-cli" # pi -> cli
+recv_topic = "from-cli" # cli -> pi
+
+# mqtt connection
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+    client.subscribe(recv_topic)
+
+def on_message(client, userdata, msg):
+    num = str(msg.payload, "utf-8")
+    sendLora(num)
+
+def _exit(rc):
+    sys.exit(rc)
+
+def send(line):
+    client.publish(send_topic, payload=line)
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("lorapi.local", 1883, 60)
+client.loop_start()
+
+
+def main():
+    global counter
+    counter = 0
+    while True:
+
+        # draw a box to clear the image
+        display.fill(0)
+        display.text('RasPi LoRa', 35, 0, 1)
+
+        receive()
+
+        display.fill(0)
+
+        display.show()
+        time.sleep(0.1)
+main()
