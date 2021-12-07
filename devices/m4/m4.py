15af7f4ae9e96f387fb5d5f493328b94ee479697
angelajt <angela@t7a.org>
2021-12-06 17:23:21 -0800

    major m4 refactor, add more classes
    - make Button, Display, Radio classes
    - refactor Order, Message classes

--- devices/m4/m4.py
+++ devices/m4/m4.py
@@ -11,261 +11,301 @@ from adafruit_display_text import label
 # TODO put this stuff in config file
 # https://docs.python.org/3/library/configparser.html
 BOARD_ID = "m4-1"
+DEST_ID = "pi"
 
-#
-# Display Setup
-#
-
-# Used to ensure the display is free in CircuitPython
+# ensure the display is free
 displayio.release_displays()
 
-# Define the pins needed for display use
-# This pinout is for a Feather M4 and may be different for other boards
-spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)  # Uses SCK and MOSI
-epd_cs = board.D9
-epd_dc = board.D10
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 
-# Set up middle button on display
-midbutton = digitalio.DigitalInOut(board.D12)
-midbutton.switch_to_input()
-midbutton.direction = digitalio.Direction.INPUT
-midbutton.pull = digitalio.Pull.UP
 
-# Create the displayio connection to the display pins
-display_bus = displayio.FourWire(
-    spi, command=epd_dc, chip_select=epd_cs, baudrate=1000000
-)
-time.sleep(1)  # Wait a bit
 
-# Create the display object - the third color is red (0xff0000)
-display = adafruit_il0373.IL0373(
-    display_bus,
-    width=296,
-    height=128,
-    rotation=270,
-    grayscale=True,
-    seconds_per_frame=10,
-    black_bits_inverted=False,
-    color_bits_inverted=False
-)
-
-# Create a display group for our screen objects
-displaygroup = displayio.Group()
-
-# Set colors
-BLACK = 0x000000
-WHITE = 0xFFFFFF
-
-FOREGROUND_COLOR = BLACK
-BACKGROUND_COLOR = WHITE
-
-# Set a background
-background_bitmap = displayio.Bitmap(296, 128, 1)
-# Map colors in a palette
-palette = displayio.Palette(1)
-palette[0] = BACKGROUND_COLOR
-# Create a Tilegrid with the background and put in the displayio group
-t = displayio.TileGrid(background_bitmap, pixel_shader=palette)
-displaygroup.append(t)
-
-txtgroup = displayio.Group(scale=3, x=20, y=40)
-displaytxt = label.Label(terminalio.FONT, text="0", color=FOREGROUND_COLOR)
-txtgroup.append(displaytxt)  # Add this text to the text group
-displaygroup.append(txtgroup)
 
 #
-# Radio Setup
+# Button
 #
 
-# Define radio parameters.
-RADIO_FREQ_MHZ = 915.0  # Frequency of the radio in Mhz. Must match your
-# module! Can be a value like 915.0, 433.0, etc.
+class Button:
+    def __init__(self):
+        # set up middle button 
+        # (physically it's on the display, but it doesn't directly
+        # interact with the display)
+        self.button = digitalio.DigitalInOut(board.D12)
+        self.button.switch_to_input()
+        self.button.direction = digitalio.Direction.INPUT
+        self.button.pull = digitalio.Pull.UP
 
-# Define pins connected to the chip, use these if wiring up the breakout according to the guide:
-CS = digitalio.DigitalInOut(board.D16)
-RESET = digitalio.DigitalInOut(board.D5)
+    def isOn(self):
+        return not self.button.value # reverse of what you'd expect :P
 
-# Initialze RFM radio
-rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
-rfm9x.tx_power = 13 # default
 
-# convert a packet to a parsed message
-def toMsg(data):
-    msg = {
-            "type": data[0],
-            "from": data[1],
-            "to": data[2],
-            "counter": data[3],
-            "ordernum": data[4]
-            }
-    return msg
+#
+# Display
+#
 
-# convert a packet to a parsed acknowledgement
-def toAck(data):
-    ack = {
-            "type": data[0],
-            "from": data[1],
-            "to": data[2],
-            "counter": data[3],
-            }
-    return ack
+class Display:
+    def __init__(self):
 
-# receive a packet over LoRa, then parse it and send an ack if needed
-def receive():
-    packet = None
-    order = None
+        # define pins
+        epd_cs = board.D9
+        epd_dc = board.D10
 
-    # check for packet rx
-    try:
-        packet = rfm9x.receive(timeout=1)
-    except RuntimeError:
-        pass
+        # create the displayio connection to the display pins
+        display_bus = displayio.FourWire(
+            spi, command=epd_dc, chip_select=epd_cs, baudrate=1000000
+        )
+        time.sleep(1)
 
-    if packet is not None:
-        print()
-        
+        # create the display object
+        self.epd = adafruit_il0373.IL0373(
+            display_bus,
+            width=296,
+            height=128,
+            rotation=270,
+            grayscale=True,
+            seconds_per_frame=10,
+            black_bits_inverted=False,
+            color_bits_inverted=False
+        )
+
+        # create a display group for our screen objects
+        self.displaygroup = displayio.Group()
+
+        # set colors
+        BLACK = 0x000000
+        WHITE = 0xFFFFFF
+
+        FOREGROUND_COLOR = BLACK
+        BACKGROUND_COLOR = WHITE
+
+        # set a background
+        background_bitmap = displayio.Bitmap(296, 128, 1)
+        # map colors in a palette
+        palette = displayio.Palette(1)
+        palette[0] = BACKGROUND_COLOR
+        # create a Tilegrid with the background and put in the displayio group
+        t = displayio.TileGrid(background_bitmap, pixel_shader=palette)
+        self.displaygroup.append(t)
+
+        self.txtgroup = displayio.Group(scale=3, x=20, y=40)
+        self.displaytxt = label.Label(terminalio.FONT, text="0", color=FOREGROUND_COLOR)
+        self.txtgroup.append(self.displaytxt)  # add this text to the text group
+        self.displaygroup.append(self.txtgroup)
+    
+    def refresh(self):
+        self.epd.show(self.displaygroup)
+        self.epd.refresh()
+
+    def update(self, order):
+        self.displaytxt.text = str(order)
+
+
+#
+# LoRa Radio
+#
+class Radio:
+    counter = 0
+
+    def __init__(self):
+        # built-in radio frequency
+        RADIO_FREQ_MHZ = 915.0
+
+        # physical pins on radio board
+        CS = digitalio.DigitalInOut(board.D16)
+        RESET = digitalio.DigitalInOut(board.D5)
+
+        self.rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
+        self.rfm9x.tx_power = 13 # default
+
+    # receive a packet over LoRa and return it
+    def receive(self, t):
+        packet = None
         try:
-            packet_text = str(packet, "utf-8")
-        except UnicodeError:
-            packet_text = str(packet)
-        
-        data = packet_text.split(", ")
-        print(data)
-
+            # acks probably need longer timeouts, so make timeout flexible
+            packet = self.rfm9x.receive(timeout=t)
+        # weird unexplained RuntimeError that we get sometimes (just ignore it and try again)
+        except RuntimeError:
+            time.sleep(0.1)
+            pass
+        return packet
+    
+    def send(self, msg):
+        m = bytes(str(msg), "utf-8")
         try:
-            msg = toMsg(data)
-        except IndexError:
-            # the message is an ack, the # of data items won't match
-            # don't bother with acks here (these get handled in sendLora)
-            return
+            self.rfm9x.send(m, destination=255, node=255)
+        # weird unexplained RuntimeError that we get sometimes (just ignore it and try again)
+        except RuntimeError: 
+            time.sleep(0.1)
+            pass
 
-        # if a packet isn't an ack and the destination is the pi, send an ack
-        # and then send the packet over MQTT (to the application)
-        if msg["to"] == BOARD_ID:
-            time.sleep(2)
-            ack = "ACK, " + BOARD_ID + ", " + msg["from"] + ", " + msg["counter"]
-            try:
-                rfm9x.send(bytes(ack, "utf-8"), destination=255, node=255)
-            except RuntimeError:
+    # receive an order over LoRa
+    def receiveOrder(self):
+        order = None
+        packet = self.receive(1)
+
+        if packet is not None:
+            msg = Message.parse(packet)
+            if msg is None: # garbage message
                 return None
-            print("ack sent!")
-            time.sleep(1)
-            order = Order(msg["ordernum"])
+            print() # make a newline for easy reading in the serial console
+            
+            if msg.isMsg(): # ensure the message is a valid order message to this board
+                self.sendAck(msg)
+                print("ack sent!")
+                order = Order(msg.ordernum)
         
-    return order
+        return order
+    
+    # send a given message over LoRa, checking for an acknowledgement
+    def sendMsg(self, msg):
+        self.counter += 1 # add to the counter
+        msg.counter = self.counter
+        gotAck = False
+        # send a message, looping over and over until we get an ack
+        while not gotAck:
 
+            self.send(msg)
+            print("sending:", msg)
+            
+            time.sleep(0.1)
+            print("message sent!")
 
-#
-# Main code
-#
+            gotAck = self.receiveAck()
+
+            if not gotAck:
+                print("no ack received! trying again") # no ack, loop again
+
+    # receive an acknowledgement over LoRa and return it
+    def receiveAck(self):
+        gotAck = False
+        packet = self.receive(5)
+
+        if packet is not None:
+            msg = Message.parse(packet)
+            if msg is None: # garbage message
+                return
+            if msg.isAck(self.counter):
+                gotAck = True
+        
+        return gotAck
+    
+    def sendAck(self, msg):
+        time.sleep(1)
+        ack = Message(type = "ACK", ordernum = msg.ordernum, status = msg.status, counter = msg.counter)
+        self.send(ack)
+        return
 
 
 class Message:
-    def __init__(self, num, status):
-        self.dest = "pi"
-        self.body = num + ", " + status
+    def __init__(self, type = "MSG", source = BOARD_ID, dest = DEST_ID, ordernum = None, status = None, counter = None):
+        self.type = type
+        self.source = source
+        self.dest = dest
+        self.counter = counter
+        self.ordernum = ordernum
+        self.status = status
 
     def __str__(self):
-        global counter
-        # counter may be useful in confirming acks 
+        return self.type + ", " + self.source + ", " + self.dest + ", " + str(self.counter) + ", " + str(self.ordernum) + ", " + self.status
 
-        return "MSG, " + BOARD_ID + ", " + self.dest + ", " + str(counter) + ", " + self.body
+    @classmethod
+    def parse(cls, packet):
+        try:
+            packet_text = str(packet, "utf-8")
+        except UnicodeDecodeError:
+            return None # garbage packet
+        print("packet text: ", packet_text)
+        data = packet_text.split(", ")
+        self = cls(
+            type = data[0],
+            source = data[1],
+            dest = data[2],
+            counter = data[3],
+            ordernum = data[4],
+            status = data[5]
+        )
+        
+        return self
+
+    def isMsg(self):
+        if self.type == "MSG" and self.dest == BOARD_ID:
+            return True
+        else:
+            return False
     
-    def send(self):
-        while True:
-            global counter
-            m = bytes(str(self), "utf-8")
-            try:
-                rfm9x.send(m, destination=255, node=255)
-            except RuntimeError:
-                time.sleep(0.1)
-                continue
-            print(self)
-            time.sleep(0.1)
-            print("Packet sent!")
+    def isAck(self, counter):
+        if self.type == "ACK" and self.dest == BOARD_ID and self.counter == str(counter):
+            return True
+        else:
+            return False
 
-            # get acknowledgement
-            packet = None
-            try:
-                packet = rfm9x.receive(timeout=5.0)
-            except RuntimeError:
-                pass
-
-            # Optionally change the receive timeout from its default of 0.5 seconds:
-            # packet = rfm9x.receive(timeout=5.0)
-            # If no packet was received during the timeout then None is returned.
-            if packet is not None:
-                packet_text = str(packet, "ascii")
-                data = packet_text.split(", ")
-                ack = toAck(data)
-
-                # data = [type (ack or msg), from, to, counter]
-                if ack["type"] == "ACK" and ack["to"] == BOARD_ID and ack["counter"] == str(counter):
-                    print("ack received!")
-                    counter += 1
-                    return # ack received, exit the function
-                    
-            print("no ack received! trying again") # no ack, loop again
+    @classmethod
+    def fromOrder(cls, order):
+        return cls(ordernum = order.num, status = order.status)
+        
 
 class Order:
-    def __init__(self, name):
-        self.num = name
+    def __init__(self, num):
+        self.num = num
         self.statusArr = ["created", "cut", "stripped", "soldered", "completed"]
         self.statusCounter = 0
         self.status = self.statusArr[self.statusCounter]
-        self.sendStatus()
+    
+    def __str__(self):
+        return self.num + " " + self.status
 
-    def updateDisplay(self):
-        displaytxt.text = self.num + " " + self.status
-        
     def updateStatus(self):
         self.statusCounter += 1
-        self.status = self.statusArr[self.statusCounter]
-        self.sendStatus()
+        try:
+            self.status = self.statusArr[self.statusCounter]
+        except IndexError: # order completed already
+            pass
 
-    def sendStatus(self):
-        msg = Message(self.num, self.status)
-        msg.send()
-        self.updateDisplay()
 
-def eventloop():
+def eventloop(lora, display, button):
     order = None
     changed = False
     while True:
         time.sleep(0.1)
-        o = receive()
+        o = lora.receiveOrder()
 
         # if we received an order number from the server ...
         if o is not None:
             changed = True
             # change the order that we're managing
             order = o
+            print() # print a newline, for easy reading on the console
+            print("new order: ", order)
             continue
             
-        # if the button is pressed ...
-        if not midbutton.value and order is not None:
-            print()
+        # if the button is pressed and we are currently managing an order ...
+        if button.isOn() and order is not None:
             changed = True
-            # send an order status update to the server
+            # update the current order's status
             order.updateStatus()
+            print() # print a newline, for easy reading on the console
+            print("updated order: ", order)
+            msg = Message.fromOrder(order)
+            lora.sendMsg(msg)
             continue
+        
+        if changed:
+            display.update(order)
 
-        if changed and display.time_to_refresh <= 0:
-            print()
-            display.show(displaygroup)
-            # display.time_to_refresh = 10
-            # print(display.time_to_refresh)
-            display.refresh()
-            print("time to refresh:", display.time_to_refresh)
-            print("display refreshed")
-            changed = False
-            continue
+            if display.epd.time_to_refresh <= 0:
+                display.refresh()
+                print()
+                print("display refreshed")
+                changed = False
+                continue
 
+        # print a series of dots on the serial console (just to know the program is running :P)
         print(".", end = '')
 
 def main():
-    global counter
-    counter = 0
-    eventloop()
+    lora = Radio()
+    display = Display()
+    button = Button()
+    eventloop(lora, display, button)
 
 main()
